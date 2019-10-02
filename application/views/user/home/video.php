fda66fd3b4b9d3e9e4a91f18aa256efa344bec23
unknown <aqibalvaaiziin@gmail.com>
2019-10-02 10:19:17 +0700

    gasido nggarap

--- application/views/user/home/video.php
+++ application/views/user/home/video.php
@@ -20,7 +20,7 @@
         <div class="modal-body mb-0 p-0">
 
           <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/A3PDXmYoF5U"
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Pkh8UtuejGw"
               allowfullscreen></iframe>
           </div>
 
